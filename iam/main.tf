b768eff803dbd8afc9f81cbf61ae291137dcc0d2
Aravind <aravindcheggoju@gmail.com>
2024-11-27 16:36:07 +0530

    iam

--- iam/main.tf
+++ iam/main.tf
@@ -1,8 +1,27 @@
-resource "aws_iam_user" "name" {
-  name = "aravind"
-  path = "/system/"
+
+
+# Create an IAM User
+resource "aws_iam_user" "iam_user" {
+  name = "aravind" # Replace with your desired username
 }
 
-resource "aws_iam" "name" {
-  
+# Attach a Managed Policy to the User (optional)
+resource "aws_iam_user_policy_attachment" "s3_full_access" {
+  user       = aws_iam_user.iam_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess" # AWS Managed Policy ARN
 }
+
+# Create Access Keys for the User (optional)
+resource "aws_iam_access_key" "example_access_key" {
+  user = aws_iam_user.iam_user.name
+}
+
+# # Output the Access and Secret Keys (optional)
+# output "access_key" {
+#   value = aws_iam_access_key.example_access_key.id
+# }
+
+# output "secret_key" {
+#   value     = aws_iam_access_key.example_access_key.secret
+#   sensitive = true
+# }
